3d76c46efa60d9227089255062ca877ccce48972
Daniel-Alencar <danielalencar746@gmail.com>
2024-09-23 10:37:59 -0300

    feat: update

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import pkg from 'whatsapp-web.js';
 const { Client, LocalAuth } = pkg;
 import qrcode from 'qrcode-terminal';
 
-import puppeteer from 'puppeteer-core';
-import formatarMensagem from './format.js';
-import atualizarOuAdicionarItem from './add.js';
-import ordenarArquivoJSON from './order.js';
-
 import generate from './gemini.js';
 import { 
   readAndFormatConversation,
@@ -18,7 +13,8 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    devtools: true
   },
   webVersionCache: { 
     type: 'remote', 
@@ -35,6 +31,7 @@ client.on('qr', (qr) => {
 // Loga no cliente
 client.on('ready', () => {
   console.log('Client is ready!');
+  new Promise(a => setTimeout(a, 5000));
 });
 
 // Responde às mensagens
@@ -59,28 +56,6 @@ client.on('message_create', async (message) => {
     await updateChat(message.from, message.from, match[1]);
     await updateChat(message.from, 'Eu', replyMessage);
   }
-    
-  // // Funções do BOT
-  // // Usando regex para extrair nome e valor
-  // const regex = /\/DAI (.+?):\s*(\d+(?:[.,]\d+)?)/;
-  // const match = message.body.match(regex);
-  
-  // if (match) {
-  //   const nome = match[1].trim();
-  //   // Substitui ',' por '.' para garantir 
-  //   // que o valor decimal seja interpretado corretamente
-  //   const valor = parseFloat(match[2].replace(',', '.'));
-  
-  //   console.log("Nome:", nome);
-  //   console.log("Valor:", valor);
-  
-  //   // Atualiza a lista
-  //   await atualizarOuAdicionarItem(nome, valor);
-  //   // Ordenar a lista
-  //   await ordenarArquivoJSON();
-  //   // Manda a mensagem formatada para o grupo
-  //   const returnMessage = await formatarMensagem();
-  //   message.reply(returnMessage);
 });
 
 // Inicializa o cliente
